ff105b6247db27011ea568e1b720cc71fc78546f
jasjas24 <banaszkm@gmail.com>
2026-05-18 13:43:43 +0200

    push

--- Public/PHP/dashboard.php
+++ Public/PHP/dashboard.php
@@ -34,7 +34,7 @@ $displayRole = $roleNames[$role];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard | damago Quizsystem</title>
-    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="../CSS/style.css">
 </head>
 <body class="auth-page">
 
