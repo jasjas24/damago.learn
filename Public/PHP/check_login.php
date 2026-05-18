92d85afe03301795fe70ea71c54befba671f8d8e
jasjas24 <banaszkm@gmail.com>
2026-05-18 13:37:52 +0200

    move dashboard

--- Public/PHP/check_login.php
+++ Public/PHP/check_login.php
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['user_email'] = $user['email']; // E-Mail statt Username speichern
 
-            header("Location: ../dashboard.php");
+            header("Location: dashboard.php");
             exit;
         } else {
             // Fehlerfall
