53d3b359109333f6f334b92def476ead31f3d692
PaulSchulte84 <19paulschulte84@gmail.com>
2026-05-27 09:27:32 +0200

    einen link entfernt

--- Public/PHP/host_quiz.php
+++ Public/PHP/host_quiz.php
@@ -21,7 +21,6 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quiz hosten | damago Quizsystem</title>
     <link rel="stylesheet" href="../CSS/style.css">
-    <link rel="stylesheet" href="../CSS/quiz_host.css">
 </head>
 <body class="auth-page">
 
