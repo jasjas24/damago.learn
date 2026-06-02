ca4808d84eab5f8f26d8aee76ebbce381c506a37
jasjas24 <banaszkm@gmail.com>
2026-06-02 16:13:06 +0200

    commit_feierabend

--- Public/DB/migration_avatar.sql
+++
@@ -1,7 +0,0 @@
--- Migration: Avatar-Spalte für Lobby-Spieler hinzufügen
--- Auf einer bereits bestehenden Datenbank einmalig ausführen.
-
-USE `damago_quiz`;
-
-ALTER TABLE `lobby_players`
-  ADD COLUMN `avatar` varchar(100) DEFAULT NULL AFTER `player_name`;
